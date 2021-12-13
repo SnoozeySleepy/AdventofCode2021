379f7bb7d69cde1b2646bf71aa6b299f5fc7af32
Christian <rednoise007@gmail.com>
2021-12-12 18:17:05 -0800

    Simplfy code for part 2, runs faster

--- day12.py
+++ day12.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 
-from collections import Counter
-
-# return True if all small caves in the path are only visited once
-def unique(path):
-    # remove all large caves and start from the path list
-    p = filter(lambda x: x.islower() and x != 'start', path)
-    c = Counter(p).most_common()
-    if c: # check if small caves are only visited once
-        return c[0][1] < 2
-    return True
-
 # recursive depth first search
 # enable the special condition, one small cave can be visited twice, with part2=True
 def dfs(graph, start, end, path, part2):
@@ -21,15 +10,15 @@ def dfs(graph, start, end, path, part2):
         return []
     paths = []
     for node in graph[start]:
-        # part 1: small caves cannot only be visited once, large caves indefinitely
+        # part 1: small caves can only be visited once, large caves indefinitely
         # part2=True activates the condition for part 2
-        # keep searching for paths as long as it's a small cave, not start
-        # and no other small caves have been visited more than once: unique check
-        if not(node in path and node.islower()) or part2 and \
-            (node.islower() and node != 'start' and unique(path)):
+        if not(node in path and node.islower()): # this covers part 1 and part 2
             paths.extend(dfs(graph, node, end, path.copy(), part2))
+        # find additional paths for part 2
+        elif (node in path and node.islower()) and part2 and node != "start":
+            paths.extend(dfs(graph, node, end, path.copy(), False))
+            
     return paths
-
 
 caves = {}
 
